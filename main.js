2ec5c66b5919923ead1e26d15fe8a52abd934b43
Md Ratul Hossain <ratulnt631@gmail.com>
2022-09-22 02:25:44 +0600

    Change th && to ,

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-const moves= [ "Rock", "Paper", "Scissors"];
-        function getComputerchoice (moves) {
-          return moves[Math.floor (Math.random ()* moves.length)].toLowerCase();
-        }
-    let playerSelection= prompt("Choose Rock, Paper or Scissors", "Rock").toLowerCase();
-    
-    alert (playerSelection);
-    function playRound (playerSelection, getComputerchoice){
-      if (playerSelection==getComputerchoice){
-        alert ("TIe!")
+ let compScore=0;
+ let playerscore=0;
+
+ const moves= ["Rock", "Paper", "Scissors"];
+  
+ const getComputerchoice = (moves) =>{
+    return moves[Math.floor (Math.random ()* moves.length)].toLowerCase();
+  }
+
+  const computerSelection = getComputerchoice();
+  const playRound = (playerSelection, computerSelection)=>{
+      if (playerSelection===computerSelection){
+        return ("Tie!");
+      }
+      else if (playerSelection=="rock" && computerSelection=="scissors"){
+        return("You win Rock Crushes Scissors");
+      }
+      else if (playerSelection=="rock" && computerSelection=="paper"){
+        return("You lose Paper cover Rock");
+      }
+      else if (playerSelection=="scissors" && computerSelection=="rock"){
+        return("You lose Rock crushes Scissors");
+      }
+      else if (playerSelection=="scissors" && computerSelection=="paper"){
+        return("You win Scissors cut Paper");
+      }
+      else if (playerSelection=="paper" && computerSelection=="scissors"){
+        return("You lose Scissors cut paper");
+      }
+      else if (playerSelection=="paper" && computerSelection=="rock"){
+        return("You win Paper covers Rock");
       }
     }
+    const playerSelection="rock"
+    
+    console.log(playRound(playerSelection, computerSelection));
