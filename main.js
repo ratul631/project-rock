846f03fb75bd23d028ae52e6d79ac3a2739cc060
Md Ratul Hossain <ratulnt631@gmail.com>
2022-09-22 02:50:25 +0600

    Create Loops

--- main.js
+++ main.js
@@ -3,34 +3,34 @@
 
  const moves= ["Rock", "Paper", "Scissors"];
   
- const getComputerchoice = (moves) =>{
-    return moves[Math.floor (Math.random ()* moves.length)].toLowerCase();
-  }
-
-  const computerSelection = getComputerchoice();
+function computerChoice (moves){
+return  moves[Math.floor(Math.random()*moves.length)].toLowerCase();
+}
+let computerSelection=computerChoice(moves);
+ const playerSelection="rock";
   const playRound = (playerSelection, computerSelection)=>{
+    console.log ("1", playerSelection, "2", computerSelection)
       if (playerSelection===computerSelection){
         return ("Tie!");
       }
-      else if (playerSelection=="rock" && computerSelection=="scissors"){
+      else if (playerSelection=="rock" , computerSelection=="scissors"){
         return("You win Rock Crushes Scissors");
       }
-      else if (playerSelection=="rock" && computerSelection=="paper"){
+      else if (playerSelection=="rock" , computerSelection=="paper"){
         return("You lose Paper cover Rock");
       }
-      else if (playerSelection=="scissors" && computerSelection=="rock"){
+      else if (playerSelection=="scissors" , computerSelection=="rock"){
         return("You lose Rock crushes Scissors");
       }
-      else if (playerSelection=="scissors" && computerSelection=="paper"){
+      else if (playerSelection=="scissors" , computerSelection=="paper"){
         return("You win Scissors cut Paper");
       }
-      else if (playerSelection=="paper" && computerSelection=="scissors"){
+      else if (playerSelection=="paper" , computerSelection=="scissors"){
         return("You lose Scissors cut paper");
       }
-      else if (playerSelection=="paper" && computerSelection=="rock"){
+      else if (playerSelection=="paper" , computerSelection=="rock"){
         return("You win Paper covers Rock");
       }
-    }
-    const playerSelection="rock"
-    
+  }
+  for (let i=0; i<5;i++)
     console.log(playRound(playerSelection, computerSelection));
